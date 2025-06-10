188133934e1006f4093547a9409db3b1e94c8ecf
sachika <s.tsukeyanagi.sys23@morijyobi.ac.jp>
2025-06-10 14:08:36 +0900

    settingファイルの変更

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-3dsody*zs8@(2cgg8g2v4jsnb$16=rvw*t8ct@+lph#%*ly6p=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
